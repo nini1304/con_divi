58df56b982423b936619fcdadb7e8afa7f279507
calc <geraldine.ibieta13@gmail.com>
2023-02-26 18:00:25 -0400

    Se unio la imagen de postgres con la de spring en contenedor mediante el uso de docker compose

--- src/main/kotlin/arquitectura/software/con_divi/DemoApplication.kt
+++ src/main/kotlin/arquitectura/software/con_divi/DemoApplication.kt
@@ -16,16 +16,16 @@ class DemoApplication
         fun main(args: Array<String>) {
             runApplication<DemoApplication>(*args)
 
-            val context = LoggerFactory.getILoggerFactory() as LoggerContext
-
-            try {
-                val configurator = JoranConfigurator()
-                configurator.context = context
-                context.reset()
-                configurator.doConfigure("src/main/kotlin/logback.xml")
-            } catch (je: JoranException) {
-                je.printStackTrace()
-            }
-            StatusPrinter.printInCaseOfErrorsOrWarnings(context)
+//            val context = LoggerFactory.getILoggerFactory() as LoggerContext
+//
+//            try {
+//                val configurator = JoranConfigurator()
+//                configurator.context = context
+//                context.reset()
+//                configurator.doConfigure("src/main/kotlin/logback.xml")
+//            } catch (je: JoranException) {
+//                je.printStackTrace()
+//            }
+//            StatusPrinter.printInCaseOfErrorsOrWarnings(context)
         }
     //}
