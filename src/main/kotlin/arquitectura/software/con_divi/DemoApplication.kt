eb50966a426e363fa1f877601a547106bdb87541
calc <geraldine.ibieta13@gmail.com>
2023-04-11 17:42:09 -0400

    Se accede a las configuraciones del servidor de configuraciones

--- src/main/kotlin/arquitectura/software/con_divi/DemoApplication.kt
+++ src/main/kotlin/arquitectura/software/con_divi/DemoApplication.kt
@@ -3,8 +3,9 @@ package arquitectura.software.con_divi
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 class DemoApplication
 fun main(args: Array<String>) {
